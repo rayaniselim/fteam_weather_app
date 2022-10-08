79bfc46063cae2d955a63fab0d2ebc4916fae7df
rayaniselim <rayaniselim@outlook.com>
2022-10-08 11:58:27 -0300

    modular

--- core_module/lib/core_module.dart
+++ core_module/lib/core_module.dart
@@ -1,5 +1,8 @@
 library core_module;
 
 export 'package:bloc/bloc.dart';
-export 'package:flutter_modular/flutter_modular.dart';
 export 'package:dio/dio.dart';
+export 'package:flutter_modular/flutter_modular.dart';
+export 'package:flutter_triple/flutter_triple.dart';
+// export 'package:model/model.dart'; // ESTA DANDO AMBIGUIDADE COM FLUTTER_MODULAR 
+
