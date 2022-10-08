30eb6985afcb0b341157b5fe2300d3cef7078bd3
rayaniselim <rayaniselim@outlook.com>
2022-10-08 12:03:40 -0300

    modular

---
+++ app_weather/lib/app/modules/home/home_page.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:flutter_triple/flutter_triple.dart';
+
+import 'home_store.dart';
+
+class HomePage extends StatefulWidget {
+  final String title;
+  const HomePage({Key? key, this.title = 'Home'}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late final HomeStore store;
+
+  @override
+  void initState() {
+    super.initState();
+    store = Modular.get<HomeStore>();
+  }
+
+  @override
+  void dispose() {
+    Modular.dispose<HomeStore>();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Counter'),
+      ),
+      body: ScopedBuilder<HomeStore, Exception, int>(
+        store: store,
+        onState: (_, counter) {
+          return Padding(
+            padding: const EdgeInsets.all(10),
+            child: Text('$counter'),
+          );
+        },
+        onError: (context, error) => const Center(
+          child: Text(
+            'Too many clicks',
+            style: TextStyle(color: Colors.red),
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          store.increment();
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
