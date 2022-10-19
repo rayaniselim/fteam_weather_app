fac2b1f2cf27ca4806a4c683f5745d008984531e
rayaniselim <rayaniselim@outlook.com>
2022-10-19 11:44:20 -0300

    feature

--- core_module/test/core_module_test.dart
+++ core_module/test/core_module_test.dart
@@ -1,1 +1,26 @@
+import 'package:flutter_test/flutter_test.dart';
 
+// class Mock?Repository extends Mock implements ?Repository{}
+void main() {}
+
+
+
+
+
+
+
+
+// // vai estar dentro da funcao que vai testar 
+// test('texto dizendo o que deve retornar ', () {
+
+// // arrange: organiza o teste, configura algumas coisas, retornos de comportamentos
+
+
+
+
+// // actions: vai chamar os metodos que tem que executar (Açao)
+
+
+
+// // asserts: vai verificar se o teste está funcionando / respondendo da forma correta
+// });}
