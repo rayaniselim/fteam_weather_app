a2991463a25d9ceab453c9476a8e2a5fcba191fe
rayaniselim <rayaniselim@outlook.com>
2022-10-28 19:45:14 -0300

    reaproveitamento widgets

--- web_weather/lib/web_weather.dart
+++ web_weather/lib/web_weather.dart
@@ -1,3 +1,3 @@
 export 'package:web_weather/home_page_web.dart';
 
-export 'package:web_weather/widgets/modal/components/app_bar_web.dart';
+export 'package:design_system/src/widgets/web/modal_web/components_web/app_bar_web.dart';
