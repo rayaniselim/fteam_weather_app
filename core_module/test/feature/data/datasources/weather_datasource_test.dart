fe6b87d2434927b6591d85902c421c78ee6bea10
rayaniselim <rayaniselim@outlook.com>
2022-11-30 14:38:19 -0300

    teste dataSource

--- core_module/test/feature/data/datasources/weather_datasource_test.dart
+++ core_module/test/feature/data/datasources/weather_datasource_test.dart
@@ -1,15 +1,29 @@
 import 'package:core_module/core_module.dart';
 
 import 'package:mocktail/mocktail.dart';
-
-/// Testa o HttpClient
+import 'package:test/expect.dart';
+import 'package:test/scaffolding.dart';
 
 class DioClientAdapterMock extends Mock implements DioClientAdapter {}
 
 void main() {
-  CustomResponse response;
-  DioClientAdapter dio;
-  WeatherRepository repository;
-  WeatherDatasource datasource;
-  IHttpClient iHttpClient;
+  CustomResponse customResponse =
+      CustomResponse(data: 'data', statusCode: 200, statusMessage: 'exito');
+  WeatherDatasource datasource =
+      WeatherDatasource(client: DioClientAdapter(Dio()));
+
+  test('O remoteSearchWeather deve retornar null e um WeatherDatasource',
+      () async {
+    final remoteSearchWeather =
+        await datasource.remoteSearchWeather(city: 'Curitiba');
+
+    expect(remoteSearchWeather, null);
+    expect(datasource, isA<WeatherDatasource>());
+  });
+
+  test('customResponse deve retornar o statusCode int?', () {
+    final responseCode = customResponse.statusCode;
+    expect(responseCode, isA<int?>());
+    print(responseCode);
+  });
 }
